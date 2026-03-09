fb237bf3e3e78cd066cd874c07ef17cd726df61e
towabii <towa.soko0406@gmail.com>
2026-03-09 17:51:55 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,41 @@
 const CACHE_NAME = 'smartbox-v19';
+
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './smartbox.jpeg',
-  'https://unpkg.com/html5-qrcode',
-  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
-  'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest',
-  'https://cdn.jsdelivr.net/npm/@tensorflow-models/mobilenet@latest',
-  'https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,1,0'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/smartbox.jpeg'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
+      .then(cache => {
         return cache.addAll(urlsToCache);
       })
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
